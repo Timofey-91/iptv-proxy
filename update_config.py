82ec32c047115de4f42e96698829c23cdfc61626
Timofey <50114291+Timofey-91@users.noreply.github.com>
2026-08-30 16:28:19 +0500

    Update update_config.py

--- update_config.py
+++ update_config.py
@@ -11,7 +11,7 @@ CONFIG_FILE = "config.json"
 def get_token():
     """Получаем access_token с PeersTV"""
     url = "http://api.peers.tv/auth/2/token"
-    payload = "grant_type=inetra%3Aanonymous&client_id=29783051&client_secret=b4d4eb438d760da95f0acb5bc6b5c760"
+    payload = "grant_type=inetra%3Aanonymous&client_id=52654316&client_secret=fa42da34d580acb321a41bcbb5998a67"
     headers = {"User-Agent": USER_AGENT, "Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(url, data=payload, headers=headers, timeout=8)
     if response.status_code != 200:
