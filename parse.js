d27233a2c6a489a694eb1c178155ada5f89869a0
Timofey <50114291+Timofey-91@users.noreply.github.com>
2026-09-23 15:51:21 +0500

    Update parse.js

--- parse.js
+++ parse.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 async function parseStream() {
   // Проверьте точный URL страницы (например: http://rodnoetv.com/mosfilm.html)
-  const TARGET_URL = 'http://rodnoetv.com/smotret/msflmgold'; 
+  const TARGET_URL = 'http://free.guljaj.com/online/?id=&idk=278'; 
 
   try {
     const response = await fetch(TARGET_URL, {
